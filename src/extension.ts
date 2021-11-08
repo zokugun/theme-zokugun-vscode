30bcaf80709038abccb39074538a739eb4bfe317
Baptiste Augrain <daiyam@zokugun.org>
2021-11-08 14:50:02 +0100

    feat: is a browser extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,63 @@
-import * as vscode from 'vscode'
+import vscode from 'vscode';
+import pkg from '../package.json';
 
-const pkg = require('../package.json')
+const CONFIG_KEY = 'zokugun-theme';
+const VERSION_KEY = 'version';
 
-const VERSION_ID = `${pkg.publisher}/${pkg.name}/version`
-
-async function showWhatsNewMessage(version: string) {
+async function showWhatsNewMessage(version: string) { // {{{
 	const actions: vscode.MessageItem[] = [{
-		title: 'Homepage'
+		title: 'Homepage',
 	}, {
-		title: 'Release Notes'
+		title: 'Release Notes',
 	}];
 
 	const result = await vscode.window.showInformationMessage(
-		`${pkg.displayName} has been updated to v${version} — check out what's new!`,
-		...actions
+		`"Zokugun Themes has been updated to v${version} — check out what's new!`,
+		...actions,
 	);
 
-	if(result != null) {
+	if(result !== null) {
 		if(result === actions[0]) {
 			await vscode.commands.executeCommand(
 				'vscode.open',
-				vscode.Uri.parse(`${pkg.homepage}`)
+				vscode.Uri.parse(`${pkg.homepage}`),
 			);
 		}
 		else if(result === actions[1]) {
 			await vscode.commands.executeCommand(
 				'vscode.open',
-				vscode.Uri.parse(`${pkg.homepage}/blob/master/CHANGELOG.md`)
+				vscode.Uri.parse(`${pkg.homepage}/blob/master/CHANGELOG.md`),
 			);
 		}
 	}
-}
+} // }}}
 
-export async function activate(context: vscode.ExtensionContext) {
-	const previousVersion = context.globalState.get<string>(VERSION_ID);
+export async function activate(context: vscode.ExtensionContext): Promise<void> { // {{{
+	const previousVersion = context.globalState.get<string>(VERSION_KEY);
 	const currentVersion = pkg.version;
 
-	if(previousVersion === undefined || currentVersion !== previousVersion) {
-		await showWhatsNewMessage(currentVersion);
+	const config = vscode.workspace.getConfiguration(CONFIG_KEY);
 
-		context.globalState.update(VERSION_ID, currentVersion);
+	if(previousVersion === undefined || currentVersion !== previousVersion) {
+		void context.globalState.update(VERSION_KEY, currentVersion);
+
+		const notification = config.get<string>('notification');
+
+		if(previousVersion === undefined) {
+			// don't show notification on install
+		}
+		else if(notification === 'major') {
+			if(currentVersion.split('.')[0] > previousVersion.split('.')[0]) {
+				void showWhatsNewMessage(currentVersion);
+			}
+		}
+		else if(notification === 'minor') {
+			if(currentVersion.split('.')[0] > previousVersion.split('.')[0] || (currentVersion.split('.')[0] === previousVersion.split('.')[0] && currentVersion.split('.')[1] > previousVersion.split('.')[1])) {
+				void showWhatsNewMessage(currentVersion);
+			}
+		}
+		else if(notification !== 'none') {
+			void showWhatsNewMessage(currentVersion);
+		}
 	}
-};
+} // }}}
